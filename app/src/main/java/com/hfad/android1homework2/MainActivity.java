64fb8287b4a89f061267f2288e523e4845ab84a2
bakaiismailov <ismailov.time@gmail.com>
2020-11-28 12:57:29 +0600

    third

--- app/src/main/java/com/hfad/android1homework2/MainActivity.java
+++ app/src/main/java/com/hfad/android1homework2/MainActivity.java
@@ -2,7 +2,6 @@ package com.hfad.android1homework2;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
